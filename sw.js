bc07834e93e1b868914f26b688a11c5bcf4be7e5
sweetshair <lovejonestherapy@gmail.com>
2026-07-22 13:43:48 -0400

    Refresh cache after splash fix

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Love Jones Therapy • Relationship IQ
 // Service worker with versioned cache (bump VERSION when you deploy changes)
 
-const VERSION = "ljt-riq-v10";
+const VERSION = "ljt-riq-v11";
 const CACHE_NAME = `${VERSION}-cache`;
 
 const ASSETS = [
