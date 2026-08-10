7f3764a379e8cca217238201aafb8c9c8bf56309
sweetshair <lovejonestherapy@gmail.com>
2026-08-09 23:42:43 -0400

    Make instrumentals feel like a live band

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // First Option Dating • Relationship IQ
 // Service worker with versioned cache (bump VERSION when you deploy changes)
 
-const VERSION = "fod-riq-v23";
+const VERSION = "fod-riq-v24";
 const CACHE_NAME = `${VERSION}-cache`;
 
 const ASSETS = [
