c65e78472a3618d97b5bb50cb21d746ccdb70339
sweetshair <lovejonestherapy@gmail.com>
2026-09-21 17:52:38 -0400

    Refresh installed app for picture first profile flow

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // First Option Dating • Relationship IQ
 // Service worker with versioned cache (bump VERSION when you deploy changes)
 
-const VERSION = "fod-riq-v35";
+const VERSION = "fod-riq-v36";
 const CACHE_NAME = `${VERSION}-cache`;
 
 const ASSETS = [
